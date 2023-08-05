e1fe566952180736737850b3ef3204aa38cfeac3
Zeeshan926 <zeeshannaveed893@gmail.com>
2023-08-05 04:21:08 -0700

    Add project files.

---
+++ Data/MVCLibrary1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCLibrary1.Models;
+
+namespace MVCLibrary1.Data
+{
+    public class MVCLibrary1Context : DbContext
+    {
+        public MVCLibrary1Context (DbContextOptions<MVCLibrary1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCLibrary1.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCLibrary1.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Tittle { get; set; }
+        public string CallNumber { get; set; }
+        public string Author { get; set; }
+    }
+}
